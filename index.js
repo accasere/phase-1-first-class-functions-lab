ea5dc6d709fada0d43aee3cd169e34cdc3659b74
Antony Caseres <accasere@outlook.com>
2022-04-10 18:56:23 -0500

    Completed Assigment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution in this file!
+
+const returnFirstTwoDrivers = driversArray => driversArray.slice(0,2) 
+
+const returnLastTwoDrivers = driversArray => driversArray.slice(-2)
+
+const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers]
+
+const createFareMultiplier = (integer) => (fare) => (integer * fare)
+
+const fareDoubler = createFareMultiplier(2)
+
+const fareTripler = createFareMultiplier(3)
+
+const selectDifferentDrivers= (arrayOfDrivers,returnDrivers) => returnDrivers(arrayOfDrivers)
